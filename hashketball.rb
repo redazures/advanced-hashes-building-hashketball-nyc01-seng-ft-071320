4deb6c3f23e4fc15928ca1415216f7471985951e
james wu <jwu_nyc@yahoo.com>
2020-06-30 03:05:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,12 +7,12 @@ def game_hash
       players:{
         player_name:"Alan Anderson",
         number: "Reggie Evans",
-        shoe:16
-        points:22
-        rebounds:12
-        assists:12
-        steals:3
-        blocks:1
+        shoe:16,
+        points:22,
+        rebounds:12,
+        assists:12,
+        steals:3,
+        blocks:1,
         slam_dunks:1
       }
     }#,
