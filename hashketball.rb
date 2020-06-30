7a4c4181be372bace14cd4d4945c148485517c59
james wu <jwu_nyc@yahoo.com>
2020-06-30 03:08:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,17 +4,8 @@ def game_hash
     home:{
       team_name: "Brooklyn Nets",
       colors:["Black","White"],
-      players:{
-        player_name:"Alan Anderson",
-        number: "Reggie Evans",
-        shoe:16,
-        points:22,
-        rebounds:12,
-        assists:12,
-        steals:3,
-        blocks:1,
-        slam_dunks:1
-      }
+      players:[]
+
     }#,
     # :away{
     #   team_name:,
