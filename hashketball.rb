8f5138e97b447f3256513b3f5e701b211838c3d1
james wu <jwu_nyc@yahoo.com>
2020-06-30 03:23:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -68,25 +68,25 @@ def game_hash
       players:[
         {
           player_name:"Jeff Adrien",
-          number:0,
-          shoe:16,
-          points:22,
-          rebounds:12,
-          assists:12,
-          steals:3,
-          blocks:1,
-          slam_dunks:1
+          number:4,
+          shoe:18,
+          points:10,
+          rebounds:1,
+          assists:1,
+          steals:2,
+          blocks:7,
+          slam_dunks:2
         },
         {
-          player_name:"Alan Anderson",
+          player_name:"Bismack Biyombo",
           number:0,
           shoe:16,
-          points:22,
-          rebounds:12,
-          assists:12,
-          steals:3,
-          blocks:1,
-          slam_dunks:1
+          points:12,
+          rebounds:4,
+          assists:7,
+          steals:22,
+          blocks:15,
+          slam_dunks:10
         },
         {
           player_name:"Alan Anderson",
