e30d49b20eca44d39cb8c854be3f7ea0598b94b3
james wu <jwu_nyc@yahoo.com>
2020-06-30 03:22:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -67,7 +67,7 @@ def game_hash
       colors:["Turquoise", "Purple"],
       players:[
         {
-          player_name:"Alan Anderson",
+          player_name:"Jeff Adrien	",
           number:0,
           shoe:16,
           points:22,
