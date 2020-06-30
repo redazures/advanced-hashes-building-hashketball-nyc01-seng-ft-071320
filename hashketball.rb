d4774a58232e9104c41d8ef54f4e91e7aa0eee93
james wu <jwu_nyc@yahoo.com>
2020-06-30 03:24:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -100,26 +100,26 @@ def game_hash
           slam_dunks:5
         },
         {
-          player_name:"Alan Anderson",
-          number:0,
-          shoe:16,
-          points:22,
-          rebounds:12,
-          assists:12,
-          steals:3,
+          player_name:"Ben Gordon",
+          number:8,
+          shoe:15,
+          points:33,
+          rebounds:3,
+          assists:2,
+          steals:1,
           blocks:1,
-          slam_dunks:1
+          slam_dunks:0
         },
         {
-          player_name:"Alan Anderson",
-          number:0,
-          shoe:16,
-          points:22,
+          player_name:"Kemba Walker",
+          number:33,
+          shoe:15,
+          points:6,
           rebounds:12,
           assists:12,
-          steals:3,
-          blocks:1,
-          slam_dunks:1
+          steals:7,
+          blocks:5,
+          slam_dunks:12
         },
       ]
     }
